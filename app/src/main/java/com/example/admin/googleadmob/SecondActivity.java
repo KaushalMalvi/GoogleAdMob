7e977e297776f1521ed2785d83fa3ce85f623ae2
KaushalMalvi <malvikaushal@gmail.com>
2016-11-11 17:33:41 +0530

    Done.

---
+++ app/src/main/java/com/example/admin/googleadmob/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.googleadmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
